41f0e003bb40b8716d86271a0a85e899c10c93c9
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-04-28 10:04:54 -0700

    Initial commit

---
+++ js/tradeStalker.js
@@ -0,0 +1,11 @@
+$( document ).ready( function(){
+
+    // get all stocks from data base 
+
+    // every minute check if database has changed.. stocks sold or bought
+
+    // every minute generate chart of stocks and get current price... adjust tables values.
+
+
+
+});
