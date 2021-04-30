a3cea27e741eda4c1f573eaddb5a402ca91c4a3d
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-04-30 08:38:44 -0700

    increase size of password to max size of hash

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE trader(
     email VARCHAR(75),
-    password VARCHAR(200),
+    password VARCHAR(255),
     PRIMARY KEY(email)
 );
 CREATE TABLE Stock(
     email VARCHAR(75),
     ticker VARCHAR(10),
-    quanity int,
+    quantity int,
     purchasePrice decimal(10,2),
     currency VARCHAR(3),
     PRIMARY KEY(email, ticker),
