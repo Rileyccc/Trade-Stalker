e4edb1ea452820e2932fa246997c0a7fb160cfa9
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-04-30 07:18:53 -0700

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE trader(
+    email VARCHAR(75),
+    password VARCHAR(200),
+    PRIMARY KEY(email)
+)
+CREATE TABLE AppVersion(
+    email VARCHAR(75),
+    ticker VARCHAR(10),
+    quanity int,
+    purchasePrice decimal(10,2),
+    currency VARCHAR(3),
+    PRIMARY KEY(email, ticker),
+    FOREIGN KEY (email) REFERENCES trader(email)
+	    ON DELETE CASCADE ON UPDATE CASCADE
+);
