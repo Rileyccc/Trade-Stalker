6ba84e5a572ac5cd698d3cd4a9e645a68011de10
Rileyccc <50608798+Rileyccc@users.noreply.github.com>
2021-05-04 19:43:48 -0700

    add crypto

--- php/addStock.php
+++ php/addStock.php
@@ -17,11 +17,17 @@ $quantity = $_POST["quantity"];
 $purchasePrice = $_POST["purchasePrice"];
 $currency = $_POST["currency"];
 
+if(isset($_POST["crypto"])){
+    $crypto = 1;
+}else{
+    $crypto = 0;
+}
+
 // create connection
 $conn = createConnection();
 // add stock to database
-$stmt = $conn->prepare("INSERT INTO stock (email, ticker, quantity, purchasePrice, currency) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("sssss", $email, $ticker, $quantity, $purchasePrice, $currency);
+$stmt = $conn->prepare("INSERT INTO stock (email, ticker, quantity, purchasePrice, currency, crypto) VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("ssssss", $email, $ticker, $quantity, $purchasePrice, $currency, $crypto);
 if($stmt->execute()){
     $conn->close();
     exit("success");
